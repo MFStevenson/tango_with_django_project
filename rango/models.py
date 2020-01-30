7d8ca48f4564adef5b90c71e67252e3841da197a
Mia Stevenson <2382850s@student.gla.ac.uk>
2020-01-30 12:25:45 +0000

    chapt 5 completed

--- rango/models.py
+++ rango/models.py
@@ -3,6 +3,12 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique = True)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.name
 
@@ -14,3 +20,4 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
