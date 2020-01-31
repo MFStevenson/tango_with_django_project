e15100af4da1a19a4a009e74715070d8d8c43851
Mia Stevenson <2382850s@student.gla.ac.uk>
2020-01-31 17:24:25 +0000

    fixed urls

--- populate_rango.py
+++ populate_rango.py
@@ -14,25 +14,33 @@ def populate():
 
     python_pages = [
         {'title': 'Official Python Tutorial',
-        'url': 'http://docs.python.org/3/tutorial/'},
+        'url': 'http://docs.python.org/3/tutorial/',
+        'views': 25},
         {'title': 'How to Think like a Computer Scientist',
-        'url': 'http://www.greenteapress.com/thinkpython/'},
+        'url': 'http://www.greenteapress.com/thinkpython/',
+        'views': 54},
         {'title': 'Learn Python in 10 Minutes',
-        'url': 'http://www.korokithakis.net/tutorials/python/'}]
+        'url': 'http://www.korokithakis.net/tutorials/python/',
+        'views': 32}]
 
     django_pages = [
         {'title': 'Official Django Tutorial',
-        'url': 'http://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+        'url': 'http://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+        'views': 78},
         {'title': 'Django Rocks',
-        'url': 'http://www.djangorocks.com/'},
+        'url': 'http://www.djangorocks.com/',
+        'views': 62},
         {'title': 'How to Tango with Django',
-        'url': 'http://www.tangowithdjango.com/'}]
+        'url': 'http://www.tangowithdjango.com/',
+        'views': 43}]
 
     other_pages = [
         {'title': 'Bottle',
-        'url': 'http://bottlepy.org/docs/dev/'},
+        'url': 'http://bottlepy.org/docs/dev/',
+        'views': 18},
         {'title': 'Flask',
-        'url': 'http://flas.pocoo.org'}]
+        'url': 'http://flask.pocoo.org',
+        'views': 57}]
 
     cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
         'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
